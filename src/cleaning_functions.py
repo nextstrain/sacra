52c80c9b2491fc8bd161b2af6a81442868290ab1
Barney Potter <barney.potter.24@gmail.com>
2017-07-27 14:00:10 -0700

    Update cleaning functions.

--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -18,7 +18,7 @@ def fix_accession(key, doc, remove):
     '''
     if 'accession' in doc and doc['accession'] is not None:
         doc['accession'] = doc['accession'].encode('ascii', 'replace')
-	doc['accession'] = doc['accession'].lower()
+        doc['accession'] = doc['accession'].lower()
         if doc['accession'].startswith('epi'):
             pass
         else:
@@ -61,34 +61,34 @@ def fix_age(doc, *args):
     Combine gisaid age information into one age field
     '''
     if 'age' in doc.keys():
-	if doc['age'].endswith('y') or doc['age'].endswith('m') or doc['age'].endswith('d'):
-	    continue
-	else:
-	    try:
-		str(doc['age'])
-	    except:
-		print "Could not parse %s as a host age." % doc['age']
-		doc['age'] = None
-	    else:
-		doc['age'] = doc['age'] + 'y'
+        if doc['age'].endswith('y') or doc['age'].endswith('m') or doc['age'].endswith('d'):
+            continue
+        else:
+            try:
+                str(doc['age'])
+            except:
+                print "Could not parse %s as a host age." % doc['age']
+                doc['age'] = None
+            else:
+                doc['age'] = doc['age'] + 'y'
     else:
-	temp_age, temp_age_unit = None, None
-	doc['age'] = None
-	if 'Host_Age' in doc:
-	    try:
-		temp_age = str(int(float(doc['Host_Age'])))
-	    except:
-		pass
-	    del doc['Host_Age']
-	if 'Host_Age_Unit' in doc:
-	    if isinstance(doc['Host_Age_Unit'], basestring):
-		temp_age_unit = doc['Host_Age_Unit'].lower()
-	    else:
-		temp_age_unit = 'y'
-	    del doc['Host_Age_Unit']
-	if isinstance(temp_age, basestring) and isinstance(temp_age_unit, basestring):
-	    doc['age'] = temp_age + temp_age_unit
-	return doc
+        temp_age, temp_age_unit = None, None
+        doc['age'] = None
+        if 'Host_Age' in doc:
+            try:
+                temp_age = str(int(float(doc['Host_Age'])))
+            except:
+                pass
+            del doc['Host_Age']
+        if 'Host_Age_Unit' in doc:
+            if isinstance(doc['Host_Age_Unit'], basestring):
+                temp_age_unit = doc['Host_Age_Unit'].lower()
+            else:
+                temp_age_unit = 'y'
+            del doc['Host_Age_Unit']
+        if isinstance(temp_age, basestring) and isinstance(temp_age_unit, basestring):
+            doc['age'] = temp_age + temp_age_unit
+        return doc
 
 ##################
 # Accessory fxns #
