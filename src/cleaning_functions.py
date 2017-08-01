9ae38d7647fd403eadb20e607a0d3d88339068e2
Barney Potter <barney.potter.24@gmail.com>
2017-08-01 14:31:13 -0700

    Generalize category of virus passages.

--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -90,6 +90,41 @@ def fix_age(doc, *args):
             doc['age'] = temp_age + temp_age_unit
         return doc
 
+def determine_passage_category(doc, *args):
+    '''
+    Separate passage into general categories, update document with determined category or None
+    Regex borrowed from McWhite et al. 2016
+    '''
+    if 'passage' in doc and doc['passage'] is not None:
+        passage = doc['passage'].upper()
+        passage_category = "undetermined"
+        if re.search(r'AM[1-9]|E[1-9]|AMNIOTIC|EGG|EX|AM_[1-9]', passage):   # McWhite
+            passage_category = "egg"
+        elif re.search(r'AM-[1-9]|EMBRYO|E$', passage):
+            passage_category = "egg"
+        elif re.search(r'LUNG|P0|OR_|ORIGINAL|CLINICAL|DIRECT', passage):    # McWhite
+            passage_category = "unpassaged"
+        elif re.search(r'ORGINAL|ORIGNAL|CLINCAL|THROAT|PRIMARY|NASO|AUTOPSY|BRONCHIAL|INITIAL|NASAL|NOSE|ORIG|SWAB', passage):
+            passage_category = "unpassaged"
+        elif re.search(r'TMK|RMK|RHMK|RII|PMK|R[1-9]|RX', passage):    # McWhite
+            passage_category = "cell"
+        elif re.search(r'S[1-9]|SX|SIAT|MDCK|MCDK|C[1-9]|CX|M[1-9]|MX|X[1-9]|^X_$', passage):  # McWhite
+            passage_category = "cell"
+        elif re.search(r'C_[1-9]|C [1-9]|MD[1-9]|MK[1-9]|MEK[1-9]', passage):
+            passage_category = "cell"
+        elif re.search(r'[Cc][Ee][Ll][Ll]', passage):
+            passage_category = "cell"
+        elif re.search(r'^S[1-9]_$| ^SX_$|SIAT2_SIAT1|SIAT3_SIAT1', passage):    # McWhite
+            passage_category = "cell"
+        elif re.search(r'UNKNOWN|UNDEFINED|NOT SPECIFIED|DIFFERENT ISOLATION SOURCES', passage):
+            pass
+        doc['passage_category'] = passage_category
+    else:
+        doc['passage_category'] = None
+
+    if 'passage' in doc:
+        doc.pop('passage',None)
+
 ##################
 # Accessory fxns #
 ##################
@@ -226,37 +261,3 @@ def format_country(self, v):
                 v['location'] = assignment[0]
                 v['division'] = assignment[1]
                 v['country'] = assignment[2]
-
-def format_passage(self, doc, initial_field, new_field, **kwargs):
-    # TODO
-    '''
-    Separate passage into general categories
-    Regex borrowed from McWhite et al. 2016
-    '''
-    if initial_field in doc and doc[initial_field] is not None:
-        passage = doc[initial_field].upper()
-        passage_category = "undetermined"
-        if re.search(r'AM[1-9]|E[1-9]|AMNIOTIC|EGG|EX|AM_[1-9]', passage):   # McWhite
-            passage_category = "egg"
-        elif re.search(r'AM-[1-9]|EMBRYO|E$', passage):
-            passage_category = "egg"
-        elif re.search(r'LUNG|P0|OR_|ORIGINAL|CLINICAL|DIRECT', passage):    # McWhite
-            passage_category = "unpassaged"
-        elif re.search(r'ORGINAL|ORIGNAL|CLINCAL|THROAT|PRIMARY|NASO|AUTOPSY|BRONCHIAL|INITIAL|NASAL|NOSE|ORIG|SWAB', passage):
-            passage_category = "unpassaged"
-        elif re.search(r'TMK|RMK|RHMK|RII|PMK|R[1-9]|RX', passage):    # McWhite
-            passage_category = "cell"
-        elif re.search(r'S[1-9]|SX|SIAT|MDCK|MCDK|C[1-9]|CX|M[1-9]|MX|X[1-9]|^X_$', passage):  # McWhite
-            passage_category = "cell"
-        elif re.search(r'C_[1-9]|C [1-9]|MD[1-9]|MK[1-9]|MEK[1-9]', passage):
-            passage_category = "cell"
-        elif re.search(r'[Cc][Ee][Ll][Ll]', passage):
-            passage_category = "cell"
-        elif re.search(r'^S[1-9]_$| ^SX_$|SIAT2_SIAT1|SIAT3_SIAT1', passage):    # McWhite
-            passage_category = "cell"
-        elif re.search(r'UNKNOWN|UNDEFINED|NOT SPECIFIED|DIFFERENT ISOLATION SOURCES', passage):
-            pass
-        doc[new_field] = passage_category
-    else:
-        doc[initial_field] = None
-        doc[new_field] = None
