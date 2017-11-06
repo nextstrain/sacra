b437bba4fec5a0d75c87e7eaa3f0186ee0fd4169
Trevor Bedford <trevor@bedford.io>
2017-11-06 13:26:30 -0800

    Cleanup test script

--- schema/test_update.py
+++ schema/test_update.py
@@ -4,8 +4,10 @@ import rethinkdb as r
 
 # Parameters
 replicates = 100
-additional_documents = 100
-seqlength = 100
+strain_count = 1000
+samples_per_strain = 10
+sequences_per_sample = 10
+seqlength = 20000
 
 # Load data
 with open('schema_zika.json') as json_data:
@@ -15,13 +17,11 @@ with open('schema_zika_nested_dict.json') as json_data:
     nested_data = json.load(json_data)
 
 # Connect to RethinkDB
-# rethink_host = os.environ['RETHINK_HOST']
-# auth_key = os.environ['RETHINK_AUTH_KEY']
 rethink_host = 'localhost'
 auth_key = ''
 conn = r.connect(host=rethink_host, port=28015, auth_key=auth_key)
 
-# Create db and create tables, only run on the first go
+# Create db, create tables and create secondary indexes, only run on the first go
 # r.db_create('relational').run(conn)
 # r.db_create('nested').run(conn)
 # r.db('relational').table_create('strains').run(conn)
@@ -30,8 +30,6 @@ conn = r.connect(host=rethink_host, port=28015, auth_key=auth_key)
 # r.db('relational').table_create('attributions').run(conn)
 # r.db('nested').table_create('strains').run(conn)
 # r.db('nested').table_create('attributions').run(conn)
-
-# Create secondary indexes
 # r.db('relational').table('samples').index_create('strain_name').run(conn)
 # r.db('relational').table('sequences').index_create('strain_name').run(conn)
 # r.db('relational').table('sequences').index_create('sample_name').run(conn)
@@ -40,6 +38,7 @@ conn = r.connect(host=rethink_host, port=28015, auth_key=auth_key)
 # r.db('relational').table('sequences').index_wait('sample_name').run(conn)
 
 # Reset data
+print("Reset data")
 r.db('relational').table('strains').delete().run(conn)
 r.db('relational').table('samples').delete().run(conn)
 r.db('relational').table('sequences').delete().run(conn)
@@ -47,7 +46,8 @@ r.db('relational').table('attributions').delete().run(conn)
 r.db('nested').table('strains').delete().run(conn)
 r.db('nested').table('attributions').delete().run(conn)
 
-# Add relational data
+# Add data
+print("Load data from schema")
 for strain in relational_data["strains"]:
     call = r.db('relational').table('strains').insert(strain).run(conn)
 for sample in relational_data["samples"]:
@@ -56,14 +56,13 @@ for sequence in relational_data["sequences"]:
     call = r.db('relational').table('sequences').insert(sequence).run(conn)
 for attribution in relational_data["attributions"]:
     call = r.db('relational').table('attributions').insert(attribution).run(conn)
-
-# Add nested data
 for strain in nested_data["strains"]:
     call = r.db('nested').table('strains').insert(strain).run(conn)
 for attribution in nested_data["attributions"]:
     call = r.db('nested').table('attributions').insert(attribution).run(conn)
 
 # Make sequences big
+print("Make sequences big")
 bigseq = "".join(['a' for x in range(0, seqlength)])
 call = r.db('relational').table('sequences').get('USVI/1/2016|VI1.1|1').update({"sequence": bigseq}).run(conn)
 call = r.db('relational').table('sequences').get('USVI/1/2016|VI1.2|1').update({"sequence": bigseq}).run(conn)
@@ -103,30 +102,54 @@ call = (r.db('nested').table('strains')
     }).run(conn))
 
 # Adding documents
+print("Adding documents")
+
+print("Adding strains to relational db")
+docs = []
 doc = r.db('relational').table('strains').get('USVI/1/2016').run(conn)
-for x in range(0, additional_documents):
+for x in range(0, strain_count):
     doc['id'] = str(x)
     doc['strain_name'] = str(x)
-    doc['samples'] = [str(x)+"|"+str(x)]
-    call = r.db('relational').table('strains').insert(doc).run(conn)
+    doc['samples'] = []
+    for y in range(0, samples_per_strain):
+        doc['samples'].append(str(x)+"|"+str(y))
+    docs.append(doc)
+call = r.db('relational').table('strains').insert(docs).run(conn)
+
+print("Adding samples to relational db")
+docs = []
 doc = r.db('relational').table('samples').get('USVI/1/2016|VI1.1').run(conn)
-for x in range(0, additional_documents):
-    doc['id'] = str(x)+"|"+str(x)
-    doc['strain_name'] = str(x)
-    doc['sample_name'] = str(x)+"|"+str(x)
-    doc['sequences'] = [str(x)+"|"+str(x)+"|"+str(x)]
-    call = r.db('relational').table('samples').insert(doc).run(conn)
+for x in range(0, strain_count):
+    for y in range(0, samples_per_strain):
+        doc['id'] = str(x)+"|"+str(y)
+        doc['strain_name'] = str(x)
+        doc['sample_name'] = str(x)+"|"+str(y)
+        doc['sequences'] = []
+        for z in range(0, sequences_per_sample):
+            doc['sequences'].append(str(x)+"|"+str(y)+"|"+str(z))
+        docs.append(doc)
+call = r.db('relational').table('samples').insert(docs).run(conn)
+
+print("Adding sequences to relational db")
+docs = []
 doc = r.db('relational').table('sequences').get('USVI/1/2016|VI1.1|1').run(conn)
-for x in range(0, additional_documents):
-    doc['id'] = str(x)+"|"+str(x)+"|"+str(x)
-    doc['strain_name'] = str(x)
-    doc['sample_name'] = str(x)+"|"+str(x)
-    doc['sequence_accession'] = str(x)+"|"+str(x)+"|"+str(x)
-    call = r.db('relational').table('sequences').insert(doc).run(conn)
+for x in range(0, strain_count):
+    for y in range(0, samples_per_strain):
+        for z in range(0, sequences_per_sample):
+            doc['id'] = str(x)+"|"+str(y)+"|"+str(z)
+            doc['strain_name'] = str(x)
+            doc['sample_name'] = str(x)+"|"+str(y)
+            doc['sequence_accession'] = str(x)+"|"+str(y)+"|"+str(z)
+            docs.append(doc)
+call = r.db('relational').table('sequences').insert(doc).run(conn)
+
+print("Adding strains to nested db")
+docs = []
 doc = r.db('nested').table('strains').get('USVI/1/2016').run(conn)
-for x in range(0, additional_documents):
+for x in range(0, strain_count):
     doc['id'] = str(x)
-    call = r.db('nested').table('strains').insert(doc).run(conn)
+    docs.append(doc)
+call = r.db('nested').table('strains').insert(doc).run(conn)
 
 # call = r.db('relational').table('strains').count().run(conn)
 # print(call)
@@ -151,37 +174,15 @@ print("Testing write performance")
 # Updating sample attribute
 print("Update sample attribute ct")
 
-# Relational durable update
-start_time = time.time()
-for x in range(0,replicates):
-    call = r.db('relational').table('samples').get('USVI/1/2016|VI1.1').update({"ct": str(x)}, durability="hard").run(conn)
-end_time = time.time()
-elapsed = (end_time - start_time)/float(replicates)
-print("relational updates per second, durable: %.1f" % float(1/elapsed))
-
-# Relational fast update
+# Relational update
 start_time = time.time()
 for x in range(0,replicates):
     call = r.db('relational').table('samples').get('USVI/1/2016|VI1.1').update({"ct": str(x)}, durability="soft").run(conn)
 end_time = time.time()
 elapsed = (end_time - start_time)/float(replicates)
-print("relational updates per second, fast: %.1f" % float(1/elapsed))
+print("relational updates per second: %.1f" % float(1/elapsed))
 
-# Nested durable update
-start_time = time.time()
-for x in range(0,replicates):
-    call = (r.db('nested').table('strains')
-        .get('USVI/1/2016')
-        .update(
-            {'samples': {'USVI/1/2016|VI1.1': {'ct' : str(x)}}},
-            durability="hard"
-        )
-        .run(conn))
-end_time = time.time()
-elapsed = (end_time - start_time)/float(replicates)
-print("nested updates per second, durable: %.1f" % float(1/elapsed))
-
-# Nested fast update
+# Nested update
 start_time = time.time()
 for x in range(0,replicates):
     call = (r.db('nested').table('strains')
@@ -193,47 +194,20 @@ for x in range(0,replicates):
         .run(conn))
 end_time = time.time()
 elapsed = (end_time - start_time)/float(replicates)
-print("nested updates per second, fast: %.1f" % float(1/elapsed))
+print("nested updates per second: %.1f" % float(1/elapsed))
 
 # Updating sequence attribute
 print("Update sequence attribute sequence_locus")
 
-# Relational durable update
-start_time = time.time()
-for x in range(0,replicates):
-    call = r.db('relational').table('sequences').get('USVI/1/2016|VI1.1|1').update({"sequence_locus": str(x)}, durability="hard").run(conn)
-end_time = time.time()
-elapsed = (end_time - start_time)/float(replicates)
-print("relational updates per second, durable: %.1f" % float(1/elapsed))
-
-# Relational fast update
+# Relational update
 start_time = time.time()
 for x in range(0,replicates):
     call = r.db('relational').table('sequences').get('USVI/1/2016|VI1.1|1').update({"sequence_locus": str(x)}, durability="soft").run(conn)
 end_time = time.time()
 elapsed = (end_time - start_time)/float(replicates)
-print("relational updates per second, fast: %.1f" % float(1/elapsed))
+print("relational updates per second: %.1f" % float(1/elapsed))
 
-# Nested durable update
-start_time = time.time()
-for x in range(0,replicates):
-    call = (r.db('nested').table('strains')
-        .get('USVI/1/2016')
-        .update({
-            'samples': {
-                'USVI/1/2016|VI1.1': {
-                    'sequences': {
-                        'USVI/1/2016|VI1.1|1' : {'sequence_locus': str(x)}
-                    }
-                }
-            }
-        }, durability="hard")
-        .run(conn))
-end_time = time.time()
-elapsed = (end_time - start_time)/float(replicates)
-print("nested updates per second, durable: %.1f" % float(1/elapsed))
-
-# Nested fast update
+# Nested update
 start_time = time.time()
 for x in range(0,replicates):
     call = (r.db('nested').table('strains')
@@ -250,7 +224,7 @@ for x in range(0,replicates):
         .run(conn))
 end_time = time.time()
 elapsed = (end_time - start_time)/float(replicates)
-print("nested updates per second, fast: %.1f" % float(1/elapsed))
+print("nested updates per second: %.1f" % float(1/elapsed))
 
 # Testing read performance
 print("Testing read performance")
