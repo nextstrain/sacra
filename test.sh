a527768d5ba3f28f7283afd310874d5d55f4467f
James Hadfield <jh22@sanger.ac.uk>
2018-05-23 14:27:29 -0700

    add expected test output; text.sh diffs output against this

--- test.sh
+++ test.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
-python src/run.py -f test/input/zika_test.fasta -o test/output/zika_test.json --pathogen zika --use_entrez_to_improve_data
+WERE_ERRORS=0
+
+function commandFailed {
+  echo -e "****** WARNING: Command on line $1 failed. ******"
+  WERE_ERRORS=1
+}
+
+# TRAPS
+trap 'commandFailed $LINENO' ERR
+
+
+echo "Running zika test using entrez"
+python src/run.py -f test/input/zika_test.fasta -o test/output/zika_test.json --pathogen zika --use_entrez_to_improve_data > test/test.log 2>&1
+
+echo "Diffing against expected output"
+diff -s test/expected-output/zika_test.json test/output/zika_test.json > /dev/null
+
+if [[ $WERE_ERRORS -eq 1 ]]; then
+  echo "Test script finished with errors."
+  exit 2
+else
+  echo "Test script finished without errors."
+
+fi
